837758b124fc1812ea5e3ebb0c62422163cd2e57
rsshonjoydas <rsshonjoydas@gmail.com>
2023-03-05 14:52:48 +0600

    ci(client): sass with tailwind css

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Children } from '@/types';
+import '../styles/index.scss';
 
 export default function RootLayout({ children }: Children) {
   return (
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <>
-      <h2>RS Shonjoy</h2>
+      <h2 className='text-primary'>RS Shonjoy - Roboto</h2>
+      <h1 className='font-cursive text-secondary text-2xl'>RS Shonjoy - Cursive</h1>
+      <h2 className='font-poppins text-primary'>RS Shonjoy - Poppins</h2>
+      <h2 className='font-montserrat text-secondary'>RS Shonjoy - Montserrat</h2>
     </>
   );
 }
